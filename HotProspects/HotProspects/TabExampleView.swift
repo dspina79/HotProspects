1bd43b7006de867f13e90f4595bea8a0d0261444
Dave Spina <dspina79@gmail.com>
2021-02-02 22:16:52 -0500

    Combined and tested environment variables and tab views

--- HotProspects/HotProspects/TabExampleView.swift
+++ HotProspects/HotProspects/TabExampleView.swift
@@ -7,26 +7,64 @@
 
 import SwiftUI
 
+class BookDetails: ObservableObject {
+    @Published var bookName = ""
+    @Published var author = ""
+    @Published var rating: Int = 0
+    @Published var review = ""
+}
+
+struct BookInfo: View {
+    @EnvironmentObject var book: BookDetails
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Book Title", text: $book.bookName)
+                TextField("Author", text: $book.author)
+            }.navigationBarTitle(Text("Book Information"))
+        }
+    }
+}
+
+struct BookRating: View {
+    @EnvironmentObject var book: BookDetails
+    
+    var body: some View {
+        NavigationView {
+            Form{
+                Text("\(book.bookName)")
+                    .font(.headline)
+                Text("\(book.author)")
+                    .font(.subheadline)
+                Stepper("Rating", value: $book.rating, in: (0...4))
+                Text("Rating: \(book.rating)")
+                TextField("Review", text: $book.review)
+            }.navigationBarTitle("Book Review")
+        }
+    }
+}
+
 struct TabExampleView: View {
-    @State private var selectedTab = 0
+    @State private var selectedTab = "Info"
+    let book = BookDetails()
+    
     var body: some View {
         TabView(selection: $selectedTab) {
-            Text("Hello")
-                .onTapGesture {
-                    self.selectedTab = 1
-                }
+            BookInfo()
                 .tabItem {
-                    Image(systemName: "1.circle.fill")
-                    Text("First")
+                    Image(systemName: "info.circle.fill")
+                    Text("Information")
                 }
-            Text("World")
+                .tag("Info")
+            BookRating()
                 .tabItem {
-                    Image(systemName: "2.circle.fill")
-                    Text("Second")
+                    Image(systemName: "star.fill")
+                    Text("Review")
             }
-            .tag(1)
+            .tag("Review")
             
-        }
+        }.environmentObject(book)
     }
 }
 
