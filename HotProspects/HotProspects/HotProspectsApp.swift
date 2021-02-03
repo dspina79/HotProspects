96d96477a5e867aa8b3c26cbb34ce5c0be09c76d
Dave Spina <dspina79@gmail.com>
2021-02-02 20:24:00 -0500

    Initial project creation

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Dave Spina on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
