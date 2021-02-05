57584b6a0e7bf56fdd1c5ee6e2b61ade3e4b8a23
Dave Spina <dspina79@gmail.com>
2021-02-04 20:10:14 -0500

    Implemented example view that included a context menu

---
+++ HotProspects/HotProspects/ContextMenuExampleView.swift
@@ -0,0 +1,47 @@
+//
+//  ContextMenuExampleView.swift
+//  HotProspects
+//
+//  Created by Dave Spina on 2/4/21.
+//
+
+import SwiftUI
+
+struct ContextMenuExampleView: View {
+    @State private var backgroundColor = Color.red
+    var body: some View {
+        VStack {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button(action: {
+                        self.backgroundColor = Color.red
+                    }) {
+                        Text("Red")
+                    }
+                    Button(action: {
+                        self.backgroundColor = Color.green
+                    }) {
+                        Text("Green")
+                    }
+                
+                    Button(action: {
+                        self.backgroundColor = Color.blue
+                    }) {
+                        Text("Blue")
+                    }
+                }
+                
+        }
+    }
+}
+
+struct ContextMenuExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContextMenuExampleView()
+    }
+}
