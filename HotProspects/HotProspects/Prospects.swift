85d0859fe6e3c540356a1f6632a6101dcbaa0d0f
Dave Spina <dspina79@gmail.com>
2021-02-06 17:42:43 -0500

    Added toggle for contacted state

--- HotProspects/HotProspects/Prospects.swift
+++ HotProspects/HotProspects/Prospects.swift
@@ -11,7 +11,7 @@ class Prospect: Identifiable, Codable {
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
 }
 
 
@@ -20,5 +20,10 @@ class Prospects: ObservableObject {
     
     init() {
         self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
     }
 }
